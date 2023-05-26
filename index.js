5858994695ae7c882069119da15b0fd760aaacfa
Anthony Febre <anthonyfebre@Anthonys-MacBook-Air.local>
2023-05-26 16:16:05 -0600

    First Code

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-console.log("suckaniggadickorsomething");
+const puppeteer = require('puppeteer');
 
+async function main() {
+    // 1. Launch a new browser
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    // 2. Go to LinkedIn
+    await page.goto('https://www.linkedin.com/login');
+
+    // 3. Input username and password to login form and submit
+    await page.type('#username', 'your_username');
+    await page.type('#password', 'your_password');
+    await page.click('.btn__primary--large');
+
+    // 4. Wait for page navigation after login
+    await page.waitForNavigation();
+
+    // 5. Use keywords to find professionals
+    await page.goto(`https://www.linkedin.com/search/results/people/?keywords=${your_keyword}`);
+
+    // 6. Go through search results and send connection requests
+    const profiles = await page.$$('a.search-result__result-link'); // A hypothetical selector, this needs to be adjusted to the actual one
+
+    for (let profile of profiles) {
+        // Open profile in a new page
+        const profilePage = await browser.newPage();
+        await profilePage.goto(await profile.getProperty('href'));
+
+        // Send connection request
+        await profilePage.click('.connect-button-selector'); // This is a hypothetical selector, you need to replace it with the actual one
+
+        // Type in your custom message
+        await profilePage.type('.message-input-selector', 'your_introductory_message'); // This is a hypothetical selector, you need to replace it with the actual one
+
+        // Confirm connection request
+        await profilePage.click('.confirm-button-selector'); // This is a hypothetical selector, you need to replace it with the actual one
+
+        // Close profile page
+        await profilePage.close();
+    }
+
+    // 7. Close browser when finished
+    await browser.close();
+}
+
+main();
+console.log("Run")
